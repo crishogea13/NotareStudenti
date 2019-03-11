dff1289903399c0ccac412d3744179f569f0be50
Cristina Hogea <crishogea98@gmail.com>
2019-03-11 23:23:11 +0200

    Remove duplicated directory

--- JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"domain"},{"l":"domain.tests"},{"l":"repository"},{"l":"repository.inMemory"},{"l":"repository.inMemory.tests"},{"l":"service"},{"l":"service.tests"},{"l":"utils"},{"l":"validator"},{"l":"validator.tests"}]
